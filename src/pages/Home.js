68b21690fc8a6f348de3049d6d818efe908c09ef
Kartikey Shaurya <kartikeyshaurya@gmail.com>
2022-06-07 14:29:39 +0530

    final home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+    const navigate = useNavigate();
+
+    const [roomId, setRoomId] = useState('');
+    const [username, setUsername] = useState('');
+    const createNewRoom = (e) => {
+        e.preventDefault();
+        const id = uuidV4();
+        setRoomId(id);
+        toast.success('Created a new room');
+    };
+
+    const joinRoom = () => {
+        if (!roomId || !username) {
+            toast.error('ROOM ID & username is required');
+            return;
+        }
+
+        // Redirect
+        navigate(`/editor/${roomId}`, {
+            state: {
+                username,
+            },
+        });
+    };
+
+    const handleInputEnter = (e) => {
+        if (e.code === 'Enter') {
+            joinRoom();
+        }
+    };
+    return (
+        <div className="homePageWrapper">
+            <div className="formWrapper">
+                
+                <h4 className="mainLabel">Welcome Hacker !</h4>
+                <div className="inputGroup">
+                    <input
+                        type="text"
+                        className="inputBox"
+                        placeholder="Secret Key"
+                        onChange={(e) => setRoomId(e.target.value)}
+                        value={roomId}
+                        onKeyUp={handleInputEnter}
+                    />
+                    <input
+                        type="text"
+                        className="inputBox"
+                        placeholder="Username"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
+                        onKeyUp={handleInputEnter}
+                    />
+                    <button className="btn joinBtn" onClick={joinRoom}>
+                        Join
+                    </button>
+                    <span className="createInfo">
+                    If you dont have a room Id request from Interviewer Or Create One from here &nbsp;
+                        <a
+                            onClick={createNewRoom}
+                            href=""
+                            className="createNewBtn"
+                        >
+                            new room
+                        </a>
+                    </span>
+                </div>
+            </div>
+           
+        </div>
+    );
+};
+
+export default Home;
